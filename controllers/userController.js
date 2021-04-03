ba927bb858d1ab001e6e4c075356a0aaa716196e
mokulling <72229978+mokulling@users.noreply.github.com>
2021-04-03 11:11:04 -0500

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ const db = require('../models/')
 
 module.exports = {
     getUser: function(req, res) {
+        const { id } = req.params
+        res.send(id)
 
     },
     updateUser: function(req,res) {
